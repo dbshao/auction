722b14d1e3869bda4976040a06555e7dbd7d9fda
shaodingbang <shaodingbang@qq.com>
2018-12-23 02:27:30 +0900

    星级组件的加入

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { SerachComponent } from './serach/serach.component';
+import { CarouselComponent } from './carousel/carousel.component';
+import { ProductComponent } from './product/product.component';
+import { StatsComponent } from './stats/stats.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    SerachComponent,
+    CarouselComponent,
+    ProductComponent,
+    StatsComponent
   ],
   imports: [
     BrowserModule,
